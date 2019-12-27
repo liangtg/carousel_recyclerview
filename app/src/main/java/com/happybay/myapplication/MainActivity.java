e3448e26123a0b5f54d90819d87d25b4b2792f62
liangtg <liangtiangang@syberos.com>
2019-12-27 18:25:21 +0800

    add swipe delete

--- app/src/main/java/com/happybay/myapplication/MainActivity.java
+++ app/src/main/java/com/happybay/myapplication/MainActivity.java
@@ -3,23 +3,26 @@ package com.happybay.myapplication;
 import android.app.Activity;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.happybay.myapplication.carousel.CarouselLayoutManager;
 import com.happybay.myapplication.carousel.CarouselZoomPostLayoutListener;
 import com.happybay.myapplication.carousel.CenterScrollListener;
-import java.util.Random;
 
 public class MainActivity extends Activity {
     RecyclerView view;
     Toast toast;
+    private TestAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +36,8 @@ public class MainActivity extends Activity {
         //view.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
         view.setHasFixedSize(true);
         view.addOnScrollListener(new CenterScrollListener());
-        view.setAdapter(new TestAdapter());
+        adapter = new TestAdapter();
+        view.setAdapter(adapter);
         view.addItemDecoration(new RecyclerView.ItemDecoration() {
             @Override public void getItemOffsets(@NonNull Rect outRect, @NonNull View view,
                 @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
@@ -42,6 +46,9 @@ public class MainActivity extends Activity {
         });
         toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
         Glide.init(this, new GlideBuilder());
+        ItemTouchHelper helper = new ItemTouchHelper(new DeleteHelper());
+        helper.attachToRecyclerView(view);
+        view.setItemAnimator(new DefaultItemAnimator());
     }
 
     private class VH extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -62,21 +69,48 @@ public class MainActivity extends Activity {
     }
 
     private class TestAdapter extends RecyclerView.Adapter<VH> {
-        private Random random = new Random();
+        private SparseIntArray array = new SparseIntArray(10);
+
+        public TestAdapter() {
+            reset();
+        }
+
+        private void reset() {
+            array.clear();
+            for (int i = 0; i < 10; i++) {
+                array.append(i, getResources().getIdentifier("i" + (i + 1), "drawable",
+                    getPackageName()));
+            }
+        }
 
         @NonNull @Override public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             return new VH(getLayoutInflater().inflate(R.layout.item_co, parent, false));
         }
 
         @Override public void onBindViewHolder(@NonNull VH holder, int position) {
-            holder.itemView.setBackgroundColor(random.nextInt() | 0xFF000000);
-            int id = getResources().getIdentifier("i" + (position + 1), "drawable",
-                getPackageName());
-            Glide.with(MainActivity.this).load(id).into(holder.image);
+            Glide.with(MainActivity.this).load(array.valueAt(position)).into(holder.image);
         }
 
         @Override public int getItemCount() {
-            return 10;
+            return array.size();
+        }
+    }
+
+    private class DeleteHelper extends ItemTouchHelper.Callback {
+
+        @Override public int getMovementFlags(@NonNull RecyclerView recyclerView,
+            @NonNull RecyclerView.ViewHolder viewHolder) {
+            return makeMovementFlags(0, ItemTouchHelper.UP | ItemTouchHelper.DOWN);
+        }
+
+        @Override public boolean onMove(@NonNull RecyclerView recyclerView,
+            @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            adapter.array.removeAt(viewHolder.getAdapterPosition());
+            adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
         }
     }
 }
